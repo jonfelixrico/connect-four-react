2ab8b3bd9faef04ddcb53701428d995ad2dfeb90
Felix <jonfelixrico@gmail.com>
2021-11-24 01:04:44 +0800

    Make the grid component emit onClick

--- src/grid/Grid.tsx
+++ src/grid/Grid.tsx
@@ -1,31 +1,51 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */ // enable this later
+import classNames from 'classnames'
 import { FC } from 'react'
 import { Player } from './player.enum'
 
 interface CellProps {
   player: Player | null
+  onClick: () => void
 }
-const Cell: FC<CellProps> = ({ player }) => {
-  if (player ?? true) {
-    // TODO transparent thing
-    return <div>?</div>
+const Cell: FC<CellProps> = ({ player, onClick: origOnClick }) => {
+  const isVacant = player ?? true
+  const onClick = (): void => {
+    if (isVacant) {
+      return
+    }
+
+    origOnClick()
   }
 
-  // TODO disc thing
-  return <div>{player}</div>
+  return (
+    <div className="flex justify-center items-center Cell container">
+      <div
+        onClick={onClick}
+        role="button"
+        aria-label="cell"
+        tabIndex={0}
+        className={classNames('h-50 w-50 disk', {
+          'player-1': player === Player.PLAYER_1,
+          'player-2': player === Player.PLAYER_2,
+        })}
+      />
+    </div>
+  )
 }
 
 interface ColumnRowsProps {
   rows: Array<Player>
+  onClick: (rowIndex: number) => void
 }
 
 /**
  * Represents a column
  */
-const ColumnRows: FC<ColumnRowsProps> = ({ rows }) => (
+const ColumnRows: FC<ColumnRowsProps> = ({ rows, onClick }) => (
   <div className="flex flex-col gap-y-1">
-    {rows.map((rowCell, index) => (
+    {rows.map((row, rowIndex) => (
       // eslint-disable-next-line react/no-array-index-key
-      <Cell key={index} player={rowCell} />
+      <Cell key={rowIndex} player={row} onClick={() => onClick(rowIndex)} />
     ))}
   </div>
 )
@@ -36,22 +56,27 @@ export interface GridProps {
    * Inner array: rows
    */
   grid: Array<Array<Player>>
+  onClick: (colIndex: number, rowIndex: number) => void
 }
 
 /**
  * Represents a connect 4 grid that consists of seven columns
  * with six rows each.
  */
-export const Grid: FC<GridProps> = ({ grid: columns }) => {
+export const Grid: FC<GridProps> = ({ grid: columns, onClick }) => {
   if (columns.length !== 7 || !columns.every((rows) => rows.length === 6)) {
     throw new Error('Invalid grid dimensions!')
   }
 
   return (
     <div className="flex flex-row gap-x-1">
-      {columns.map((rows, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <ColumnRows rows={rows} key={index} />
+      {columns.map((rows, colIndex) => (
+        <ColumnRows
+          rows={rows}
+          // eslint-disable-next-line react/no-array-index-key
+          key={colIndex}
+          onClick={(rowIndex) => onClick(colIndex, rowIndex)}
+        />
       ))}
     </div>
   )
