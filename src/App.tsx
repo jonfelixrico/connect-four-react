28ab1b2eff178b7a5abbc4ca6e950073c10dc180
Felix <jonfelixrico@gmail.com>
2021-11-25 00:12:29 +0800

    Implement history snapshots

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import { cloneDeep } from 'lodash'
+import { cloneDeep, uniqueId } from 'lodash'
 import { FC, useCallback, useState } from 'react'
 import { Grid } from './grid/Grid'
 import { Player } from './grid/player.enum'
+
+type Grid = Array<Array<Player>>
+interface HistoryEntry {
+  grid: Grid
+  id: string
+}
 
 const INIT_GRID: Array<Array<Player>> = new Array(7)
   .fill(null)
   .map(() => new Array(6).fill(null))
 
+const INIT_HISTORY: HistoryEntry[] = [
+  {
+    grid: INIT_GRID,
+    id: uniqueId(),
+  },
+]
+
 export const App: FC = () => {
   const [grid, setGrid] = useState(INIT_GRID)
   const [player, setPlayer] = useState(Player.PLAYER_1)
+  const [history, setHistory] = useState(INIT_HISTORY)
 
   const onClick = useCallback(
     (colIdx: number): void => {
@@ -30,17 +44,36 @@ export const App: FC = () => {
           `Player ${player} has placed a token on (${colIdx}, ${firstNullIdx}).`
         )
 
+        setPlayer(
+          player === Player.PLAYER_1 ? Player.PLAYER_2 : Player.PLAYER_1
+        )
+        setHistory((historyState) => [
+          ...historyState,
+          {
+            grid: clone,
+            id: uniqueId(),
+          },
+        ])
+
         return clone
       })
-
-      setPlayer(player === Player.PLAYER_1 ? Player.PLAYER_2 : Player.PLAYER_1)
     },
     [player]
   )
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
-      <Grid grid={grid} onClick={onClick} />
+    <div className="h-screen w-screen">
+      <div>
+        <Grid grid={grid} onClick={onClick} />
+        <div
+          style={{ transform: 'scale(0.1)' }}
+          className="pointer-events-none flex flex-row"
+        >
+          {history.map((historyEntry) => (
+            <Grid grid={historyEntry.grid} key={historyEntry.id} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
