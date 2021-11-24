54e854577e4be0933d3d7b30075e4d97b0880920
Felix <jonfelixrico@gmail.com>
2021-11-24 22:42:52 +0800

    Integrate grid with the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-import { FC } from 'react'
+import { cloneDeep } from 'lodash'
+import { FC, useCallback, useState } from 'react'
+import { Grid } from './grid/Grid'
+import { Player } from './grid/player.enum'
+
+const INIT_GRID: Array<Array<Player>> = new Array(7)
+  .fill(null)
+  .map(() => new Array(6).fill(null))
 
 export const App: FC = () => {
-  return <div>placeholder</div>
+  const [grid, setGrid] = useState(INIT_GRID)
+  const [player, setPlayer] = useState(Player.PLAYER_1)
+
+  const onClick = useCallback(
+    (colIdx: number): void => {
+      setGrid((gridState) => {
+        const clone = cloneDeep(gridState)
+        const column = clone[colIdx]
+
+        const firstNullIdx = column.findIndex((rowCell) => rowCell === null)
+        column[firstNullIdx] = player
+
+        console.debug(
+          `Player ${player} has placed a token on (${colIdx}), ${firstNullIdx}`
+        )
+
+        return clone
+      })
+
+      setPlayer(player === Player.PLAYER_1 ? Player.PLAYER_2 : Player.PLAYER_1)
+    },
+    [player]
+  )
+
+  return (
+    <div className="h-screen w-screen">
+      <Grid grid={grid} onClick={onClick} />
+    </div>
+  )
 }
