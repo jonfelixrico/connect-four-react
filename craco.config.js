67cf2669c8fdbb98572f4376d02ce2d2eb7e9c12
Felix <jonfelixrico@gmail.com>
2021-12-01 23:51:05 +0800

    Set up craco for path alias

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,24 @@ module.exports = {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
+  plugins: [
+    {
+      /*
+       * This craco-alias method is taken from this medium article:
+       * https://newbedev.com/create-react-app-typescript-3-5-path-alias
+       */
+      plugin: require('craco-alias'),
+      options: {
+        source: 'tsconfig',
+
+        // baseUrl SHOULD be specified; plugin does not take it from tsconfig
+        baseUrl: './src',
+        /*
+         * tsConfigPath should point to the file where "baseUrl" and "paths"
+         * are specified
+         */
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
 }
