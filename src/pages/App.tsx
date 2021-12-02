43648753dd4eb92339ccd0c16ebc591955417d52
Felix <jonfelixrico@gmail.com>
2021-12-03 00:31:34 +0800

    Remove unintended logging

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,6 @@ import { FC, useCallback, useEffect, useState } from 'react'
 import { Grid } from '@components/grid/Grid'
 import { Player } from '@typings/player.enum'
 import { GridMatrix } from '@typings/grid.types'
-import { evaluateGrid } from '@utils/grid-evaluator.util'
 
 interface GameState {
   grid: GridMatrix
@@ -54,7 +53,6 @@ export const App: FC = () => {
   )
 
   useEffect(() => {
-    console.log(evaluateGrid(gameState.grid))
     setHistory((historyState) => [
       ...historyState,
       {
