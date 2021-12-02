d6c73252fa74258fd288c9ad6e2035f96c06e81a
Felix <jonfelixrico@gmail.com>
2021-12-03 01:08:08 +0800

    Push placeholder for testing grid-evaluator

---
+++ src/utils/grid-evaluator.util.test.ts
@@ -0,0 +1,11 @@
+describe('Grid evaluator', () => {
+  it.todo('detects a horizontal win')
+
+  it.todo('detects a vertical win')
+
+  it.todo('detects a diagonal TL-to-BR win')
+
+  it.todo('detects a diagonal TL-to-BL win')
+})
+
+export {}
