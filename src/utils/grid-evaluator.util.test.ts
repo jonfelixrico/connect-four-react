fb62d950cff5b5378a6dbc94ebe613c1cc012da8
Felix <jonfelixrico@gmail.com>
2021-12-03 01:27:05 +0800

    Create horizontal win test

--- src/utils/grid-evaluator.util.test.ts
+++ src/utils/grid-evaluator.util.test.ts
@@ -1,8 +1,36 @@
+import { Player } from '@typings/player.enum'
 import { evaluateGrid } from './grid-evaluator.util'
 import { generateGrid } from './grid.utils'
 
+// this is a template for visualization
+/*
+ * -------
+ * -------
+ * -------
+ * -------
+ * -------
+ * -------
+ */
+
 describe('Grid evaluator', () => {
-  it.todo('detects a horizontal win')
+  it('detects a horizontal win', () => {
+    const grid = generateGrid()
+
+    /*
+     * -------
+     * -------
+     * -------
+     * -------
+     * ooo----
+     * xxxx---
+     */
+    // eslint-disable-next-line no-multi-assign
+    grid[0][0] = grid[1][0] = grid[2][0] = grid[3][0] = Player.PLAYER_1
+    // eslint-disable-next-line no-multi-assign
+    grid[0][1] = grid[1][1] = grid[2][1] = Player.PLAYER_2
+
+    expect(evaluateGrid(grid)).toBe(Player.PLAYER_1)
+  })
 
   it.todo('detects a vertical win')
 
@@ -11,9 +39,7 @@ describe('Grid evaluator', () => {
   it.todo('detects a diagonal TL-to-BL win')
 
   it('detects if there is no win', () => {
-    const grid = generateGrid()
-    const winner = evaluateGrid(grid)
-
-    expect(winner).toBe(null)
+    const grid = generateGrid() // empty grid
+    expect(evaluateGrid(grid)).toBe(null)
   })
 })
