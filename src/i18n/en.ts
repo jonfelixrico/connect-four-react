c610b948917c5e32b5a0326a81deab36f821cd45
Felix <jonfelixrico@gmail.com>
2021-12-05 01:49:12 +0800

    Add winner strings

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -5,5 +5,10 @@ export const en = {
       player1: "Player 1's turn.",
       player2: "Player 2's turn.",
     },
+
+    win: {
+      player1: 'Player 1 has won the game.',
+      player2: 'Player 2 has won the game.',
+    },
   },
 }
