a8778a5a5b8d22efc44f654946577ca324cae3fc
Felix <jonfelixrico@gmail.com>
2021-12-05 16:41:00 +0800

    Create tests for dropDisc

--- src/utils/grid.utils.test.ts
+++ src/utils/grid.utils.test.ts
@@ -1,5 +1,7 @@
 import { GridMatrix } from '@typings/grid.types'
-import { generateGrid } from './grid.utils'
+import { Player } from '@typings/player.enum'
+import { toGridMatrix } from './grid.test-utils'
+import { ColumnFullError, dropDisc, generateGrid } from './grid.utils'
 
 describe('generateGrid', () => {
   let grid: GridMatrix
@@ -16,5 +18,47 @@ describe('generateGrid', () => {
     expect(grid.map((column) => column.length)).toStrictEqual([
       6, 6, 6, 6, 6, 6, 6,
     ])
+  })
+})
+
+describe('dropDisc', () => {
+  it('must not allow dropping to a full column', () => {
+    const initialGrid = toGridMatrix([
+      'O------',
+      'X------',
+      'O------',
+      'X------',
+      'O------',
+      'X------',
+    ])
+
+    expect(() => {
+      dropDisc(initialGrid, Player.PLAYER_1, 0)
+    }).toThrowError(ColumnFullError)
+  })
+
+  it('must drop discs to the bottom of the column', () => {
+    const beforeDrop = toGridMatrix([
+      '-------',
+      '-------',
+      '-------',
+      '--X----',
+      'O-O----',
+      'XOXOXOX',
+    ])
+
+    const dropTo = 0
+    const afterDrop = toGridMatrix([
+      '-------',
+      '-------',
+      '-------',
+      'X-X----',
+      'O-O----',
+      'XOXOXOX',
+    ])
+
+    expect(dropDisc(beforeDrop, Player.PLAYER_1, dropTo)).toStrictEqual(
+      afterDrop
+    )
   })
 })
